86b4a9bd012ea93633ac5237f8604f9ab221042b
Sagar kumar <92781413+Sagar-138@users.noreply.github.com>
2024-05-21 23:03:11 +0530

    189. Rotate Array.java

--- 189. Rotate Array.java
+++ 189. Rotate Array.java
@@ -1,10 +1,13 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        //if value of k is larget than length of array then % is used
         k%=nums.length;
+        //reverse all elements of array
        reverse(nums, 0,nums.length-1);
+        //reverse only 0 to k elements of reversed array
        reverse(nums, 0, k-1);
-       reverse(nums, k,nums.length-1); 
-
+        //reverse k to last element of the array 
+       reverse(nums, k,nums.length-1);
     }
     public static void reverse(int [] nums, int start, int end){
         if(nums==null||nums.length==1)
