5a25841a4dfaa2c5fc8da4e763271646d3e3c69b
Sagar kumar <92781413+Sagar-138@users.noreply.github.com>
2024-08-01 21:34:57 +0530

    217. Contains Duplicate.java

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> map=new HashSet<>();
+        for(int n:nums){
+            if(map.contains(n)) return true;
+
+            map.add(n);
+        }
+        return false;
+    }
+}
